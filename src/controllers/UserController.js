954172214ace1d764bf13df46a1149c7ff8e381d
DaniloSB91 <danilosilva.barao@gmail.com>
2022-09-07 17:20:58 -0300

    Install Bcrypt do encrypt passwords, and update de userController to update user data

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,5 +1,5 @@
+const { hash } = require("bcrypt")
 const AppError = require("../utils/AppError");
-
 const sqliteConnection = require('../database/sqlite')
 
 class UserController {
@@ -18,12 +18,48 @@ class UserController {
       throw new AppError("Email já está em uso")
     }
 
-    await database.run("INSERT INTO users (name, email, password) VALUES (?,?,?)",[name, email, password])
+    const hashedPassword = await hash(password, 8)
+
+    await database.run("INSERT INTO users (name, email, password) VALUES (?,?,?)",[name, email, hashedPassword])
 
 
 
     return responde.status(201).json()
  
+  }
+
+  async update(request, responde){
+    const { name, email } = request.body;
+    const { id } = request.params;
+
+    const database = await sqliteConnection(); //Conexão com o banco
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]) // Traz as informações do banco com o ID
+    //verifica sem o usuário existe
+    if(!user){
+      throw new AppError("Usuário não encontrado")
+    }
+    
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]) 
+    
+    //Verificar se o email no banco e o ID do banco são iguais ID enviado pelos parâmetros
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== id ){
+      throw new AppError("Está email já está em uso")
+      console.log(userWithUpdatedEmail)
+    }
+
+    user.name = name;
+    user.email = email;
+
+    await database.run(`
+    UPDATE users SET
+    name = ?,
+    email = ?,
+    updated_at = ?
+    WHERE id = ?`,
+    [user.name, user.email, new Date(), id]);
+
+    return responde.status(200).json()
+
   }
 }
 
