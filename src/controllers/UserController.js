c9b4ab3e497e06cda5a58ee198058441d5eaa5ab
DaniloSB91 <danilosilva.barao@gmail.com>
2022-09-10 17:37:21 -0300

    Password update rules are updated

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,4 @@
-const { hash } = require("bcrypt")
+const { hash, compare } = require("bcrypt")
 const AppError = require("../utils/AppError");
 const sqliteConnection = require('../database/sqlite')
 
@@ -30,7 +30,7 @@ class UserController {
   // Fim do cadastro de usuário.
 
   async update(request, responde){
-    const { name, email } = request.body;
+    const { name, email, password, old_password } = request.body;
     const { id } = request.params;
 
     const database = await sqliteConnection(); //Conexão com o banco
@@ -41,18 +41,30 @@ class UserController {
     }
     // verifica se o email existe no banco. 
     const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]) 
-    console.log(userWithUpdatedEmail  )
-    
+       
     //Verifica se o email existe && Verifica se o ID do banco é igual ao ID informado
     if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id ){
       throw new AppError("Está email já está em uso")
     }
     
-    user.name = name;
-    user.email = email;
-
+    //Se as verificações passarem, os dados enviado são atualizados na Const USER
+    //nullish operator | Se a var Name estiver em branco, será utilizando dos dados do banco para atualização
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
+    //Verificar se existe a senha antiga e nova.
     if(password && !old_password){
       throw new AppError("Informar a senha antiga para definir a nova senha")
+    }
+    //Verificar se senha nova e antiga forem informados
+    if(password && old_password){
+      const checkOldPassword = await compare(old_password, user.password); //Usar o compare do Bcrypt para verificar a senha antiga informada e a senha do banco
+      
+      if(!checkOldPassword){
+        throw new AppError("A Senha antiga não confere.")
+      }
+
+      user.password = await hash(password, 8); //Se passar por todas as verificações, a senha é atualizada e criptografada
+
     }
 
 
@@ -60,9 +72,10 @@ class UserController {
     UPDATE users SET
     name = ?,
     email = ?,
-    updated_at = ?
+    password =?,
+    updated_at = DATETIME('now')
     WHERE id = ?`,
-    [user.name, user.email, new Date(), id]);
+    [user.name, user.email, user.password, id]);
 
     return responde.status(200).json()
 
